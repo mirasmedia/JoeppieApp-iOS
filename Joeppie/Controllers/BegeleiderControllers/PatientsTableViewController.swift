a2c5d5241b40646c99262b9d8b83d8dc2d443334
s.mirza <s.mirza@m2mobi.com>
2020-03-11 16:43:35 +0100

    hide badge from list and sort list by first name

--- Joeppie/Controllers/BegeleiderControllers/PatientsTableViewController.swift
+++ Joeppie/Controllers/BegeleiderControllers/PatientsTableViewController.swift
@@ -51,7 +51,8 @@ class PatientsTableViewController: UITableViewController {
                 
                 decoder.dateDecodingStrategy = .formatted(dateFormatter)
                 guard let patients = try? decoder.decode([Patient].self, from: jsonData) else { return }
-                self.patients = patients
+                self.patients = patients.sorted(by: {$0.firstName < $1.firstName})
+                // TODO : Delete print
                 print(patients)
                 self.patientsTableView.reloadData()
             })
@@ -98,55 +99,11 @@ class PatientsTableViewController: UITableViewController {
         } else {
             cell.patientNameLabel.text = "\(patient.firstName) \(patient.lastName)"
         }
+        cell.badgeImageView.isHidden = true
         
+        //Shahin : TODO Find a way to enable the badge for users
 //        cell.badgeImageView.isHidden = indexPath.row % 2 == 0
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-    
-    /*
-    // Shahin: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
