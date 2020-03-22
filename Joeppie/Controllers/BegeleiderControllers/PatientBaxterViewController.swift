bcc25302413aec63783b461c233c02caf0cd0d99
s.mirza <s.mirza@m2mobi.com>
2020-03-22 17:37:20 +0100

    ordering baxters

--- Joeppie/Controllers/BegeleiderControllers/PatientBaxterViewController.swift
+++ Joeppie/Controllers/BegeleiderControllers/PatientBaxterViewController.swift
@@ -71,6 +71,7 @@ class PatientBaxterViewController: UIViewController {
 
 
     func getBaxters(){
+
         
         ApiService.getAllBaxtersPatient(patientId: self.patient!.id)
             .responseData(completionHandler: { [weak self] (response) in
@@ -92,7 +93,12 @@ class PatientBaxterViewController: UIViewController {
                 decoder.dateDecodingStrategy = .formatted(dateFormatter)
                 
                 let rs = try? decoder.decode([Baxter].self, from: response.data!)
-                self!.baxterlist = rs!.sorted(by: {$0.dayOfWeek < $1.dayOfWeek})
+
+                self!.baxterlist = rs!.reordered()
+                
+                
+                
+                
                 self!.handleBaxters()
                 self!.tableview.dataSource = self
                 self!.tableview.delegate = self
